9dccb4f6e63dc0ea80e14a4c1df022c2c357564a
Timur Ognev <togin1712@gmail.com>
2020-11-25 12:34:13 +0300

    NavLink. Дополнение из библиотеки 'react-router-dom', позволяющая переходить на страницы, не перезагружая страницу

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 
-import './Header.css';
+import s from'./Header.module.css';
 
 const Header = () => {
     return (
         <div className="header">
             <nav>
-                <div className="nav-wrapper  blue darken-3">
+                <div className={`${s.nav_wrapper} nav-wrapper  blue darken-3`}>
                     <a href="#!" className="brand-logo">Logo</a>
                     <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
                     <ul className="right hide-on-med-and-down">
-                        <li><a href="/profile">Профиль</a></li>
-                        <li><a href="/dialogs">Сообщения</a></li>
+                        <li><NavLink to="/profile" activeClassName={s.active}>Профиль</NavLink></li>
+                        <li><NavLink to="/dialogs" activeClassName={s.active}>Сообщения</NavLink></li>
                     </ul>
                 </div>
             </nav>
 
             <ul className="sidenav" id="mobile-demo">
-                <li><a href="/profile">Профиль</a></li>
-                <li><a href="/dialogs">Сообщения</a></li>
+                <li><NavLink to="/profile" activeClassName={s.active}>Профиль</NavLink></li>
+                <li><NavLink to="/dialogs" activeClassName={s.active}>Сообщения</NavLink></li>
             </ul>
         </div>
     )
