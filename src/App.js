24752197c401195cec6f638e6bc2fd8c0d8a2780
Timur Ognev <togin1712@gmail.com>
2020-11-25 15:31:51 +0300

    Создание неархитектурных данных внутри <Dialogs />, их отображение и дополнительная разбивка внутри на компоненты

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ const App = () => {
     <BrowserRouter>
     <div className="app">    
       <Header />
-      <Route path="/profile" component={Profile} />
-      <Route path="/dialogs" component={Dialogs} />  
+      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/dialogs" component={Dialogs} />  
     </div>
     </BrowserRouter>
   );
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,6 +1,24 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Dialogs.module.css'
 
+const persons = [{id: 'rerget-435-fg-gerg-5-f',name: 'Alex'}, {id: 'rer5-fg-ger-gerg-5-f', name: 'Ahmet'}]
+const messages = [{id: 'ret-5fg-gerg-5-f', value: 'Hello, msf'}, {id: 'vhyht-rfuuk-cvcfr-44-f', value: 'What you say?'}]
+
+const MessageItem = (props) => {
+    return(
+        <div className="message_item">
+            <div className={s.her}>{props.value}</div>
+        </div>
+    )
+}
+const PersonItem = (props) => {
+    return(
+        <div className="person_item">
+            <NavLink to={props.id} className="flow-text">{props.name}</NavLink>
+        </div>
+    )
+}
 
 const Dialogs = () => {
     return (
@@ -8,12 +26,16 @@ const Dialogs = () => {
             <div className="row">
                 <div className="col s7 push-s5">
                     <div className={s.wrap_message + "flow-text center"} >
-                        <div className={s.me}>Hello</div>
-                        <div className={s.her}>Hello</div>
+                    {messages.map(item => <MessageItem value={item.value}/>)}
                     </div>
+                    <form>
+                        <textarea></textarea>
+                        <button>Send</button>
+                    </form>
+                    
                 </div>
                 <div className="col s5 pull-s7 center">
-                    <div className="flow-text">5-columns wide pulled to the left by 7-columns.</div>
+                    {persons.map(item => <PersonItem id={item.id} name={item.name}/>)}
                 </div>
             </div>
         </div>
