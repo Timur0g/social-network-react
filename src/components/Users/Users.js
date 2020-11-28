4cf4b5bc74d4f331cf8a748cef78a9adfedce5d5
Timur Ognev <togin1712@gmail.com>
2020-11-28 15:46:20 +0300

    Делаем из Users классовую компоненту

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,39 +1,43 @@
 import Axios from 'axios';
 import React from 'react';
 
-const Users = props => {
-    const side = () => {if (props.users.length === 0) {
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+
         Axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUser(response.data.items)
+            this.props.setUser(response.data.items)
         })
-    }}
-    return (
-        <div className="users">
-            <div className="col s12 m7">
-                <button onClick={side}>Показать</button>
 
-                {props.users.map(item => {
-                    return (
-                        <div className="card horizontal">
-                            <div className="card-image">
-                                <img alt='-' src={item.photos.large ? item.photos.large : 'https://image.freepik.com/free-vector/error-404-not-found-glitch-effect_8024-4.jpg'}></img>
-                            </div>
-                            <div className="card-stacked">
-                                <div className="card-content">
-                                    <h2>{item.name}</h2>
-                                    <p>{item.status}</p>
-                                </div>
-                                <div className="card-action">
-                                    {item.followed ? <a onClick={() => props.unfollow(item.id)}>Отписаться</a> : <a onClick={() => props.follow(item.id)}>Подписаться</a>}
+    }
 
+    render() {
+        return (
+            <div className="users">
+                <div className="col s12 m12">
+                    {this.props.users.map(item => {
+                        return (
+                            <div className="card horizontal small">
+                                <div className="card-image">
+                                    <img alt='-' src={item.photos.large ? item.photos.large : 'https://image.freepik.com/free-vector/error-404-not-found-glitch-effect_8024-4.jpg'}></img>
+                                </div>
+                                <div className="card-stacked">
+                                    <div className="card-content">
+                                        <h2>{item.name}</h2>
+                                        <p>{item.status}</p>
+                                    </div>
+                                    <div className="card-action">
+                                        {item.followed ? <a onClick={() => this.props.unfollow(item.id)}>Отписаться</a> : <a onClick={() => this.props.follow(item.id)}>Подписаться</a>}
+
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Users;
